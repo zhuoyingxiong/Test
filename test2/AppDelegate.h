0b8713420cd3c9e231bd5a98850587b2c58edf6f
zhuoyingxiong <zhuoyingxiong@zhuoyingxiong.local>
2016-05-06 18:46:41 +0800

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/6.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/6.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
