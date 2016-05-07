0b77829455e654ff377002647f287df8249bf3a0
zhuoyingxiong <zhuoyingxiong@zhuoyingxiong.local>
2016-05-07 17:22:14 +0800

    添加了一个homeviewcontroller

---
+++ test2/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/7.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ test2/HomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.m
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/7.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+   
+}
+@end
