81542d1ddc13e131fa8d0a5369b2bcec3a856409
zhuoyingxiong <zhuoyingxiong@zhuoyingxiong.local>
2016-05-07 16:51:31 +0800

    添加了一个model

---
+++ test2/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/7.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@end
---
+++ test2/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  test2
+//
+//  Created by zhuoyingxiong on 16/5/7.
+//  Copyright © 2016年 zhuoyingxiong. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
